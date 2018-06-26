f9b4457deebb54991a82969f5cd8ed3bdedc9f4f
Andre Farah <andrefarah5@gmail.com>
2018-06-27 09:41:52 +1000

    Comment Addition for reverseString Functions

--- RandomFunctions2/randomFunctions.cpp
+++ RandomFunctions2/randomFunctions.cpp
@@ -3,6 +3,7 @@
 
 using std::swap;
 
+// for the following 2 reverseString functions, you could add an if statement that only swaps the 2 characters if they are not the same (you would have to convert them both to lowercase before performing the comparison)
 string reverseStringForward(string stringToReverse) {
 	int oneLessStringSize = stringToReverse.size() - 1;
 	for (int i = 0; i < stringToReverse.size() / 2; i++)
